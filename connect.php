7a5f662d383294fa09ae732249032625c5537a08
Aeserverdev <159782978+Aeserverdev@users.noreply.github.com>
2024-06-18 02:51:39 +0700

    Update connect.php

--- connect.php
+++ connect.php
@@ -7,7 +7,7 @@
 	$number = $_POST['number'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','','test');
+	$conn = new mysqli('localhost','root','','daftar');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
